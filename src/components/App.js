d2f2e8da1210e046cb92d8f4f268e0154ca33b02
hukurou82 <hukurou82@gmail.com>
2021-03-01 14:40:13 +0900

    SignInコンポーネントを開かず直接Mainコンポーネントを開く処理で開発効率を上げる

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,16 @@ import logo from "../logo.svg";
 import "./App.css";
 import SignIn from "./SignIn";
 import Main from "./Main";
+import config from "../config.json";
 
-function App() {
+const App = () => {
   const [name, setName] = useState("");
-  console.log({ name });
 
-  if (name === "") {
+  if (config.signInEnabled && name === "") {
     return <SignIn setName={setName} />;
   } else {
     return <Main name={name} />;
   }
-}
+};
 
 export default App;
